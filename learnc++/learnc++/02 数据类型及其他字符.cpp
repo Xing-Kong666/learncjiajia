89cf627afce7bb00ffd190c3f112c94658efd6cd
Xing-Kong666 <3156142863@qq.com>
2026-09-08 15:45:36 +0800

    向02之中添加了字符串和布尔类型

--- learnc++/learnc++/02 数据类型及其他字符.cpp
+++ learnc++/learnc++/02 数据类型及其他字符.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 using namespace std;
+#include <string>
 
 int main() {
 	/*整型*/
@@ -16,6 +17,7 @@ int main() {
 	cout << "long的字节=  " << sizeof(long) << endl;
 	cout << "d= " << d << endl;
 	cout << "long long的字节=  " << sizeof(long long) << endl;
+	cout << "\n";
 	/*实型(浮点型)*/
 	/*单精度float7位有效数字，从整数位开始算，双精度double15-16位*/
 	/*最多显示6位有效数字，可以更改*/
@@ -25,17 +27,35 @@ int main() {
 	double f = 1.3652442;
 	cout << "f= " << f << endl;
 	cout << "double的字节= " << sizeof(double) << endl;
+	cout << "\n";
 	/*字符型*/
 	/*char,内存中以ASCII码值存放*/
 	char g = 'a';
 	cout << "g= " << g << endl;
 	cout << "g的ascii码= " << (int)g << endl;
 	cout << "char的字节= " << sizeof(char) << endl;
+	cout << "\n";
+	/*字符串类型*/
+	/*c类型*/
+	char ch[] = "hello algorithm";
+	cout << "c类型的字符串= " << ch << endl;
+	/*c++类型*/
+	string str = "hello c++";
+	cout << "c++类型的字符串= " << str << endl;
+	cout << "str的字节= " << sizeof(str) << endl;
+	cout << "string类型的字节= " << sizeof(string) << endl;
+	cout << "\n";
+	/*bool类型*/
+	bool boo = true;
+	cout << "boo= " << boo << endl;
+	boo = false;
+	cout << "boo= " << boo << endl;
+	cout << "\n";
 	/*sizeof为字节计算函数*/
 	/*科学计数法*/
 	int h = 4e2;
 	float i = 4e-2;
-	cout << "h= " << h << "i= " << i << endl;
+	cout << "h= " << h << "\ni= " << i << endl;
 	/*转义字符*/
 	/* \n换行，\t水平制表，默认为8位，\\为单个反斜线，\"和\'为引号，等等*/
 	cout << "hello\nworld\n";//endl为换行加刷新缓冲区
