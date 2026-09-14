3f3b27e06e1eae27998c14107de769de0df2870a
Xing-Kong666 <3156142863@qq.com>
2026-09-14 13:05:56 +0800

    对07跳转语句的goto语句进行了错误修正

--- learnc++/learnc++/07跳转语句.cpp
+++ learnc++/learnc++/07跳转语句.cpp
@@ -2,38 +2,39 @@
 using namespace std;
 
 int main() {
-	///*01break语句*/
-	////打印楼层数
-	//int floor = 0;
-	//while (1) {
-	//	floor++;
-	//	if (floor == 5) {
-	//		break;
-	//	}
-	//	cout << floor << endl;
-	//}
+	/*01break语句*/
+	//打印楼层数
+	int floor = 0;
+	while (1) {
+		floor++;
+		if (floor == 5) {
+			break;
+		}
+		cout << floor << endl;
+	}
 	/*02continue语句*/
 	//打印0-100，遇到5的倍数不打印
-	//int num = 0;
-	//while (num <= 100) {
-	//	if (num % 5 == 0) {
-	//		num++;
-	//		continue;
-	//	}
-	//	else {
-	//		cout << num << endl;
-	//		num++;
-	//	}
-	//}
+	int num = 0;
+	while (num <= 100) {
+		if (num % 5 == 0) {
+			num++;
+			continue;
+		}
+		else {
+			cout << num << endl;
+			num++;
+		}
+	}
 	/*03goto语句*/
 	int go = 0;
-	while (1) {
-		if (go == 101)break;
+	while (go<101) {
 		if (go % 2 == 0) goto Even;
 		else goto Odd;
+	//话题也是按顺序执行的，和case类似，需设置continue或break
 	Even:
 		cout << go * go << endl;
 		go++;
+		continue;
 	Odd:
 		cout << go * 12 << endl;
 		go++;
